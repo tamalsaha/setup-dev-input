9cb221a8dbb3a91e8594b5328479b5c3afb8c60b
Tamal Saha <tamal@appscode.com>
2024-06-24 03:06:34 -0700

    wip
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # setup-dev-input
+
+```yaml
+apiVersion: v1
+items:
+- apiVersion: cloud.appscode.com/v1alpha1
+  kind: Credential
+  metadata:
+    creationTimestamp: null
+    name: azure-credentials
+  spec:
+    name: ""
+    ownerID: 0
+    type: ""
+  status: {}
+- apiVersion: cloud.appscode.com/v1alpha1
+  kind: Credential
+  metadata:
+    creationTimestamp: null
+    name: azure-storage-credentials
+  spec:
+    name: ""
+    ownerID: 0
+    type: ""
+  status: {}
+- apiVersion: meta.k8s.appscode.com/v1alpha1
+  kind: ClusterProfile
+  metadata:
+    creationTimestamp: null
+    name: aws-dbaas
+  spec:
+    description: ""
+    title: AWS DBaaS
+kind: List
+metadata: {}
+```
--- main.go
+++ main.go
@@ -1,9 +1,67 @@
 package main
 
 import (
-	_ "go.bytebuilders.dev/resource-model/apis/cloud/v1alpha1"
-	_ "kmodules.xyz/resource-metadata/apis/meta/v1alpha1"
+	"fmt"
+	cloudv1alpha1 "go.bytebuilders.dev/resource-model/apis/cloud/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	rsmeta "kmodules.xyz/resource-metadata/apis/meta/v1alpha1"
+	"sigs.k8s.io/yaml"
 )
 
 func main() {
+	var list metav1.List
+	list.TypeMeta = metav1.TypeMeta{
+		APIVersion: "v1",
+		Kind:       "List",
+	}
+	list.Items = []runtime.RawExtension{
+		{
+			Object: &cloudv1alpha1.Credential{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Credential",
+					APIVersion: cloudv1alpha1.SchemeGroupVersion.String(),
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "azure-credentials",
+				},
+				Spec: cloudv1alpha1.CredentialSpec{},
+			},
+		},
+		{
+			Object: &cloudv1alpha1.Credential{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Credential",
+					APIVersion: cloudv1alpha1.SchemeGroupVersion.String(),
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "azure-storage-credentials",
+				},
+				Spec: cloudv1alpha1.CredentialSpec{},
+			},
+		},
+		{
+			Object: &rsmeta.ClusterProfile{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ClusterProfile",
+					APIVersion: rsmeta.SchemeGroupVersion.String(),
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "aws-dbaas",
+				},
+				Spec: rsmeta.ClusterProfileSpec{
+					Title:               "AWS DBaaS",
+					Description:         "",
+					Provider:            "",
+					RequiredFeatureSets: nil,
+				},
+			},
+		},
+	}
+
+	data, err := yaml.Marshal(list)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(data))
 }
